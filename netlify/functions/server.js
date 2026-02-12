91f8d5f525b9c191651f8dd650a42771a40d7038
Oumayma MESBAHI <oumayma.mesbahi@etu.uae.ac.ma>
2026-02-12 21:59:42 +0100

    Fix server

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,8 +1,10 @@
 import { createRequestHandler } from '@remix-run/netlify';
-import * as build from '../../build/server/index.js';
 
 export const handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
+  getLoadContext: async (event, context) => {
+    return {
+      netlifyContext: context,
+    };
+  },
 });
 
