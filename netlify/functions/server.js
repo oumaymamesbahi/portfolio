5ea9ce10731421ba74c5a939b8426223448cdabf
Fusion <fusion@builder.io>
2026-02-13 19:29:21 +0100

    Configure Vercel for Remix v2 deployment

--- netlify/functions/server.js
+++
@@ -1,6 +0,0 @@
-import { createRequestHandler } from '@remix-run/netlify';
-import * as build from '../../build/server/index.js';
-
-const handler = createRequestHandler({ build, mode: process.env.NODE_ENV });
-
-export default handler;
