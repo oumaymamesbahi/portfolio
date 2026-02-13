00843daf8345966f8bc454f3355519670315c836
Oumayma MESBAHI <oumayma.mesbahi@etu.uae.ac.ma>
2026-02-13 17:12:46 +0100

    vercel

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   assetsInclude: ['**/*.glb', '**/*.hdr', '**/*.glsl'],
   build: {
     assetsInlineLimit: 1024,
+    chunkSizeWarningLimit: 1000,
   },
   server: {
     port: 7777,
